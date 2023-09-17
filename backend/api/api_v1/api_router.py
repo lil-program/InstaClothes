eea0da97a7dfa511543c6f8c387e4dfe94f61e56
Mikami Kazuki <kazuki20029140@gmail.com>
2023-09-17 22:55:28 +0900

    フロントエンドの通信用のエンドポイントの追加

--- backend/api/api_v1/api_router.py
+++ backend/api/api_v1/api_router.py
@@ -1,9 +1,10 @@
+from api.api_v1.endpoints import closets, clothes, test, users
 from fastapi import APIRouter
-
-from api.api_v1.endpoints import users, closets, clothes
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(closets.router, prefix="/closets", tags=["closets"])
 api_router.include_router(clothes.router, prefix="/clothes", tags=["clothes"])
+
+api_router.include_router(test.router, prefix="/test", tags=["test"])
