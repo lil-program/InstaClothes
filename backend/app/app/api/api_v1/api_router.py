ccb055a1e4ea1c1548863dc3b4b2b05499dbabcc
Mikami Kazuki <kazuki20029140@gmail.com>
2023-09-20 20:54:10 +0900

    不要なエンドポイントの削除

--- backend/app/app/api/api_v1/api_router.py
+++ backend/app/app/api/api_v1/api_router.py
@@ -1,10 +1,9 @@
-from app.api.api_v1.endpoints import closets, clothes, test, users
 from fastapi import APIRouter
+
+from app.api.api_v1.endpoints import closets, clothes, users
 
 api_router = APIRouter()
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(closets.router, prefix="/closets", tags=["closets"])
 api_router.include_router(clothes.router, prefix="/clothes", tags=["clothes"])
-
-api_router.include_router(test.router, prefix="/test", tags=["test"])
