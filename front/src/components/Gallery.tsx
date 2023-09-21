ada397ac9183de6bd9f8aeed548b6f7e09e4fc08
Kaneki Ou <kaneki.king.1493@gmail.com>
2023-09-21 13:28:49 +0900

    デザイン少しやります

--- front/src/components/Gallery.tsx
+++ front/src/components/Gallery.tsx
@@ -2,6 +2,7 @@ import { ImageCard } from "./ImageCard";
 import { useState, useEffect } from "react";
 import { OpenAPI } from "../api_clients";
 import { ClothesService } from "../api_clients";
+import { Box } from "@mui/system";
 
 export function Gallery(props) {
   const { closet_id } = props;
@@ -38,17 +39,31 @@ export function Gallery(props) {
   };
 
   return (
-    <div>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
       {clothes.map((clothe) => {
         return (
-          <ImageCard
-            key={clothe.id}
-            clothe_id={clothe.id}
-            shop_url={clothe.shop_url}
-            img_path={clothe.img_path}
-            handleDelete={handleDelete}
-            handleAddClick={handleAddClick}
-          />
+          <Box
+            sx={{
+              paddingRight: 5,
+              paddingLeft: 5,
+              paddingTop: 5,
+              transition: "0.3s",
+              "&:hover": {
+                transform: "translateY(-5px)",
+                boxShadow: "0 10px 20px 0 rgba(0,0,0,0.12)",
+              },
+            }}
+          >
+            <ImageCard
+              key={clothe.id}
+              clothe_id={clothe.id}
+              shop_url={clothe.shop_url}
+              img_path={clothe.img_path}
+              handleDelete={handleDelete}
+              handleAddClick={handleAddClick}
+              boxShadow={"none"}
+            />
+          </Box>
         );
       })}
     </div>
